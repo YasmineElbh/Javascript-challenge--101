6ba7d35274b07d2bc67cc70346fc1cb51394c346
YasmineElbh <elbahiyasmine.4@gmail.com>
2024-10-19 22:00:34 +0100

    strings

--- strings/ex/main.js
+++ strings/ex/main.js
@@ -1,5 +1,20 @@
-//shoin the price of an items
+//using '..'  default
+//using `..` if we need interpolation,multi-line strings
+
+
+//showing the price of an items
 console.log('Items (' + (1 + 1) + '): $' + (2095 + 70) / 100);
 
 console.log("I'm learning js");
 console.log("I\'m learning js");
+console.log(`hello`);
+
+console.log(`Items (${1+1}): $${(2095 + 70) / 100}`);
+console.log(`yasm
+ine`);
+console.log('yasm\nine')
+
+console.log(`Total cost:${5 + 3}$`)
+
+
+
