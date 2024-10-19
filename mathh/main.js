f40afd869678fe366106353f01d0a1678b4c82a6
YasmineElbh <elbahiyasmine.4@gmail.com>
2024-10-19 21:19:40 +0100

    math

--- mathh/main.js
+++ mathh/main.js
@@ -1,10 +1,18 @@
 let x = 3.99;
 
-// x = Math.round(x);//4
-// x = Math.floor(x);//3
-// x = Math.ceil(x);//4
-// x = Math.pow(x,2);//15.92010000
-// x = Math.sqrt(x);//1.9974
-// x = Math.abs(x);//3.99
+x = Math.round(x);//4 ==> returns the nearest integer
+x = Math.floor(x);//3 ==> returns the value x rounded down to its nearest
+x = Math.ceil(x);//4 ==> returns the value x rouned up to its nearest
+x = Math.pow(x,2);//15.92010000 ==> return the value x to the power
+x = Math.sqrt(x);//1.9974 ==> return he square
+x = Math.abs(x);//3.99 ==> returns the abbsolute(positive) value of x
 
 console.log(x);
+
+let a = 5;
+let b = 9;
+let maxi,mini;
+maxi = Math.max(x,a,b);
+mini = Math.min(x,a,b);
+console.log(maxi);
+console.log(mini);
