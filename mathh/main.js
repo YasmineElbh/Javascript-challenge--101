50195990fe6c98d9579ac861fe4c075116e73032
YasmineElbh <elbahiyasmine.4@gmail.com>
2024-10-19 21:11:56 +0100

    math

---
+++ mathh/main.js
@@ -0,0 +1,10 @@
+let x = 3.99;
+
+// x = Math.round(x);//4
+// x = Math.floor(x);//3
+// x = Math.ceil(x);//4
+// x = Math.pow(x,2);//15.92010000
+// x = Math.sqrt(x);//1.9974
+// x = Math.abs(x);//3.99
+
+console.log(x);
